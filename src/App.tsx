35cd501de876e92312a1a22e4ab3fef128a5f08b
Jackalob <nike9712@gmail.com>
2022-10-09 20:46:52 +0800

    mod union content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+/* eslint-disable no-nested-ternary */
+import { useState } from 'react';
+import './App.css';
+
+type IStep = 'Detail' | 'Shipping' | 'Payment';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState<IStep>('Detail');
+  const handleNext = () => {
+    if (step === 'Detail') setStep('Shipping');
+    if (step === 'Shipping') setStep('Payment');
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <>
+      {step === 'Detail' ? (
+        <>
+          <h1>1.</h1>
+          <p>step1</p>
+        </>
+      ) : step === 'Shipping' ? (
+        <>
+          <h1>2.</h1>
+          <p>step2</p>
+        </>
+      ) : (
+        step === 'Payment' && (
+          <>
+            <h1>3.</h1>
+            <p>step3</p>
+          </>
+        )
+      )}
+      {step !== 'Payment' && (
+        <button type="button" onClick={handleNext}>
+          next
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
